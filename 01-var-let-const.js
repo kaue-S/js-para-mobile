0d2c59229ec4ff1ccd2ca4acf2542c652834c818
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-21 09:27:24 -0300

    Create 01-var-let-const.js

---
+++ 01-var-let-const.js
@@ -0,0 +1,13 @@
+var idade = 18;
+if( idade >=18 ){
+    // let mensagem = "É adulto!";
+} else {
+    // let mensagem = "É menor!";
+}
+
+console.log(idade);
+// console.log(mensagem);
+
+let meuNome = "Tiago";
+meuNome = "Tiago B. dos Santos";
+console.log(meuNome);
