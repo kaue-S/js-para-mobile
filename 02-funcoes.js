0a7955a1f37acf1f9a9405d06fdac22dfa316d49
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-21 11:12:12 -0300

    tipos de funções

--- 02-funcoes.js
+++ 02-funcoes.js
@@ -13,3 +13,53 @@ function exemplo2() {
 };
 
 exemplo2();
+
+
+// sintaxe nomeada ou declarada
+function calcular(valor1, valor2) {
+
+   return valor1 - valor2;
+};
+
+let resultado = calcular(35, 20);
+let resultado2 = calcular(70, 32);
+let resultado3 = calcular(70, 160);
+
+console.log(resultado);
+console.log(resultado2);
+console.log(Math.abs(resultado3));
+
+
+// Sintaxe Arrow function
+
+const exemplo3 = () => {
+    console.log("Arrow function");
+};
+
+exemplo3();
+
+
+// const saudacao = (cliente) => {
+//     console.log("Olá, " + cliente);
+// };
+
+// omitir os parênteses do parâmetro (somente quando for 1)
+// const saudacao = cliente => {
+//     console.log("Olá, " + cliente);
+// };
+
+// omitir as chaves somente quando for uma única instrução
+const saudacao = cliente => console.log("Olá, " +cliente);
+
+saudacao("Fulano");
+saudacao("Beltrano");
+
+const calculaMetade = (valor) => {
+    return valor / 2;
+};
+
+let = resultadoA = calculaMetade(100);
+let = resultadoB = calculaMetade(666);
+
+console.log(resultadoA);
+console.log(resultadoB);
