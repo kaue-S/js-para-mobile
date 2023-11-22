ca1aff00f7850253ea6a3a3b52e7084316340ad6
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-22 11:53:41 -0300

    Update 05-operador-spread.js

--- 05-operador-spread.js
+++ 05-operador-spread.js
@@ -2,8 +2,28 @@
 
 /* spread: espalhar, copiar elementos/dados de um array/objeto para dentro de outro array/objeto. */
 
+
+//Spread com arrays
 const bandasProg = ["Pink-floyd", "Rush", "Yes"];
 const maisBandas = [...bandasProg, "Slayer", "Nightwish"];
 
 console.log(bandasProg);
 console.log(maisBandas);
+
+//Spread com objetos
+
+const cliente = {
+    nome: "fulano",
+    idade: 25,
+    cidade: "São Paulo",
+
+};
+
+const novosDados = {
+    ...cliente,
+    estado: "Sp",
+    pedido: "1254",
+    total: 5000.28
+}
+
+console.log(novosDados);
