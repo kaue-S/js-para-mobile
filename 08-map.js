194ef160b1edfbbf646dbe39a387bae3a5df5232
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-23 10:59:39 -0300

    Update 08-map.js

--- 08-map.js
+++ 08-map.js
@@ -1,7 +1,9 @@
 // 08-map.js
+import cursos from "./modulos/cursos.js";
+
 
 /* map (mapear - transformar em outra ocisa)
-passar por elementos de um array e realiza operações em cada um deles através de uma função, gerando um novo array.
+passar por elementos de um array e realiza operações em cada um deles através de uma função (callback), gerando um novo array.
 */
 
 //exemplo 1
@@ -10,3 +12,14 @@ console.log(numeros);
 
 const numerosDobrados = numeros.map( numero => numero * 2);
 console.log(numerosDobrados);
+
+
+console.log("----------");
+
+console.log(cursos);
+
+//exemplo 2 
+
+const titulos = cursos.map( curso => curso.titulo );
+
+console.log(titulos);
