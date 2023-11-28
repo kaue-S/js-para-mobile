77e7a92432e5257968a4a7b931339aa2d02f0943
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-28 09:09:23 -0300

    Update 11-promisses-com-fetch.js

--- 11-promisses-com-fetch.js
+++ 11-promisses-com-fetch.js
@@ -3,13 +3,23 @@
 /* Comunicação assíncrona (AJAX) para 
 carregamento de dados usando fetch/then/catch */
 
-const apiUrl = `https://jsonplaceholder.typicode.com/users/3`;
+const apiUrl = `https://jsonplaceholder.typicode.com/users/12`;
 
 // Conecte/Acesse...
 fetch(apiUrl)
 
 // ... e então capture a resposta da API no formato JSON
-.then(resposta => resposta.json())
+.then(resposta => {
+
+    //Se a resposta da requisição não for bem sucedida(por exemplo, se não houver registros, ou se der erro no próprio server [500])
+    if(!resposta.ok){
+        throw new Error( 
+            `Erro na requisição: ${resposta.status} - ${resposta.statusText}`
+         )
+    }
+
+    return resposta.json();
+})
 
 // ... e então capture os dados contidos na resposta
 .then( dados => console.log(dados) )
